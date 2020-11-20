93400e9902a4811f12160bd92ff584e9cf2a4cea
Dan Kalish <daniel.kalish@automox.com>
2020-11-20 14:12:43 -0700

    The first commit to perform

---
+++ main_program.py
@@ -0,0 +1,15 @@
+"""Agglomerate prices"""
+
+
+class RunIt:
+    #
+    def __init__(self):
+        self.motors = None
+        self.props = None
+
+    def set_motors(self, motors):
+        self.motors = motors
+
+    def set_props(self, props):
+        self.props = props
+
